533c2849c728188dcaf6ba88aca7cc9c9ed14a43
Himanshu Raj <71289405+himansh231104@users.noreply.github.com>
2024-07-28 22:35:55 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  /*
+  npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
+  */
